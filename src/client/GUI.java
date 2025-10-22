dcf4f8337fa74ee0a5981af61aae28c71c93d514
miguel-albuq <masse@iscte-iul.pt>
2025-10-22 22:44:13 +0100

    inicio projeto

---
+++ src/client/GUI.java
@@ -0,0 +1,29 @@
+package client;
+
+import javax.swing.*;
+
+public class GUI {
+    private JFrame frame;
+    private JPanel panel;
+    private JButton button;
+    private JLabel label;
+
+    public GUI() {
+        frame = new JFrame("IsKahoot");
+        panel = new JPanel();
+        button = new JButton("Enviar Resposta");
+        label = new JLabel("Pergunta aparecerá aqui");
+
+        panel.add(label);
+        panel.add(button);
+        frame.add(panel);
+
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
